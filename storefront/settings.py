e3947301253b866be0760944dba8f6442e61f7d2
Ved3264 <pved81034@gmail.com>
2025-03-05 17:36:33 +0530

    create like ,store,tag app and create model also change datbase setting and connect to mysql database

--- storefront/settings.py
+++ storefront/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'playground',
+    'store',
+    'tags',
+    'like',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +79,11 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'root',
     }
 }
 
